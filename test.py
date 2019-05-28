42983ac32ddac2a45e6d6c45627f199657d29f92
divyasandeep <divyaenugutala1809@gmail.com>
2019-05-28 14:54:49 +0100

    guessing secreat number

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
-# test
+# WHILe LOOP
+
+
+guess_count =1
+while guess_count <=5:
+    print(guess_count)
+    guess_count = guess_count + 1
+print("done")
+
+
+#Guess Game
+
+secreat_number = 9
+guess_count = 0
+guess_limit = 3
+while guess_count < guess_limit:
+    guess = int (input('guess: '))
+    guess_count += 1
+    if guess == secreat_number:
+        print('you won! ')
